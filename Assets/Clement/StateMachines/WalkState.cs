f4215bfc075717f2201f5ca87501cd1709a6b8d0
Otakode <marchal.cl@gmail.com>
2019-04-27 15:31:47 +0200

    stateMachine progress
    Signed-off-by: Otakode <marchal.cl@gmail.com>

--- Assets/Clement/StateMachines/WalkState.cs
+++ Assets/Clement/StateMachines/WalkState.cs
@@ -1,11 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class WalkState : StateMachineBehaviour
 {
+	private NavMeshAgent navMeshAgent;
+
 	public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
 	{
+		navMeshAgent = animator.gameObject.GetComponent<NavMeshAgent>();
+		if (navMeshAgent == null)
+			throw new System.NotImplementedException();
 
+		navMeshAgent.destination = new Vector3(-5, 0, 0);
 	}
 }
