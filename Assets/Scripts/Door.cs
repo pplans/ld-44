58bb37b1d4e854d4cd16e6960b0507934695e538
pplans <pierre.plans@gmail.com>
2019-04-28 00:45:43 +0200

    Doors

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+	private Animator m_animator;
+
+	private void Start()
+	{
+		m_animator = GetComponent<Animator>();
+		m_animator.enabled = false;
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if(!m_animator.enabled)
+		{
+			m_animator.enabled = true;
+			m_animator.Play("OpenSingleDoor");
+			Debug.Log("Play OpenSingleDoor");
+		}
+	}
+
+	private void OnTriggerExit(Collider other)
+	{
+		m_animator.enabled = false;
+	}
+}
