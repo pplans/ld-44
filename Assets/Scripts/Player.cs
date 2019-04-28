01cbb4df8efb6818f50b9e3ff16d95455c6bbb4a
pplans <pierre.plans@gmail.com>
2019-04-28 17:48:31 +0200

    FIX PC

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -37,12 +37,16 @@ public class Player : MonoBehaviour
 
     public float Blood { get { return m_blood; } set { m_blood = System.Math.Min(value, m_max_blood); } }
 
-    public Player()
+	public void Start()
+	{
+		m_modelRenderer = m_model.GetComponent<Renderer>();
+		m_PlayerMat = new Material(m_modelRenderer.material);
+	}
+
+	public Player()
 	{
 		m_blood = 100.0f;
 		m_obfuscationTimer = m_obfuscationTimerMax;
-		m_modelRenderer = m_model.GetComponent<Renderer>();
-		m_PlayerMat = new Material(m_modelRenderer.material);
 	}
 
     void LossBloodOverTime() {
@@ -74,7 +78,7 @@ public class Player : MonoBehaviour
     {
         Debug.Log("INVISIBLE");
         isInvisible = true;
-        this.gameObject.layer = 2;
+        m_collider.enabled = false;
 		m_modelRenderer.material = m_ObfuscationMat;
 
 	}
@@ -83,7 +87,7 @@ public class Player : MonoBehaviour
     {
         Debug.Log("VISIBLE");
         isInvisible = false;
-        this.gameObject.layer = 9;
+        m_collider.enabled = true;
 		m_modelRenderer.material = m_PlayerMat;
 	}
 
