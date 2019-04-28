1002ddab3a8578c8305b999e25b5b764b1212f48
Otakode <marchal.cl@gmail.com>
2019-04-29 00:08:26 +0200

    AI scream and stun states WIP
    Signed-off-by: Otakode <marchal.cl@gmail.com>

---
+++ Assets/AI/StateMachines/StunState.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StunState : StateMachineBehaviour
+{
+	private PersonAI personAI;
+
+	public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+	{
+		personAI = animator.gameObject.GetComponent<PersonAI>();
+
+		personAI.navMeshAgent.speed = 0;
+
+		personAI.animations.SetTrigger("Die");
+	}
+
+	public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+	{
+		personAI.navMeshAgent.speed = 0;
+	}
+}
