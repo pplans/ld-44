c870a17b779636a7da02d1f648b135cf6245732b
vincent defives <vincent.defives@hotmail.fr>
2019-04-28 16:20:41 +0200

    The vampire become invisible when the player hold C. It takes more blood than usual.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,16 +8,25 @@ public class Player : MonoBehaviour
     public GameObject cam;
 
     public bool isAlive = true;
+    public bool isInvisible = false;
     public bool isEatingPeople = false;
 
     float m_blood;
     const float m_max_blood = 100f;
     const float m_bloodPerSecond = 5f; // is static
+    const float m_bloodPerSecondDuringInvisibility = 15f; // is static
 
     public float m_timeToEatOnePeople = 1f;
     public float m_distanceToEatPeople = 1f;
 
 	public Material m_uiBloodPoolMat;
+    CapsuleCollider m_collider;
+
+
+    private void Awake()
+    {
+        m_collider = this.gameObject.GetComponent<CapsuleCollider>();
+    }
 
     public float Blood { get { return m_blood; } set { m_blood = System.Math.Min(value, m_max_blood); } }
 
@@ -27,11 +36,15 @@ public class Player : MonoBehaviour
 	}
 
     void LossBloodOverTime() {
-        if (!isEatingPeople)
+        if (!isEatingPeople & !isInvisible)
         {
             m_blood -= m_bloodPerSecond * Time.deltaTime;
         }
-        
+        else if (isInvisible)
+        {
+            m_blood -= m_bloodPerSecondDuringInvisibility * Time.deltaTime;
+        }
+
         //Debug.Log("Blood : " +m_blood);
 
         if (m_blood <= 0)
@@ -44,6 +57,20 @@ public class Player : MonoBehaviour
     {
         Debug.Log("Player is dead");
         isAlive = false;
+    }
+
+    public void PlayerBecomeInvisible()
+    {
+        Debug.Log("INVISIBLE");
+        isInvisible = true;
+        m_collider.enabled = false;
+    }
+
+    public void PlayerBecomeVisible()
+    {
+        Debug.Log("VISIBLE");
+        isInvisible = false;
+        m_collider.enabled = true;
     }
 
     public void UpdatePlayer()
