c2e098d83241b94c6e5a7ebafac692ebc3f4f496
Otakode <marchal.cl@gmail.com>
2019-04-28 12:54:01 +0200

    apply player motion
    Signed-off-by: Otakode <marchal.cl@gmail.com>

--- Assets/Clement/Scripts/PlayerController.cs
+++ Assets/Clement/Scripts/PlayerController.cs
@@ -4,25 +4,26 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-	private Rigidbody rigidbody;
+	public Animator animations;
+	public Rigidbody rigidbody;
 	private Vector2 inputDirection;
-
-	public void Start()
-	{
-		rigidbody = GetComponent<Rigidbody>();
-	}
 
 	public void Update()
 	{
 		inputDirection = new Vector2(UnityEngine.Input.GetAxis("Horizontal"), UnityEngine.Input.GetAxis("Vertical"));
 		inputDirection.Normalize();
-		
+
+		Vector3 move = inputDirection.magnitude * transform.forward * 5f;
+
+		//Synchronize motion
+		float forwardVelocity = move.magnitude;
+		animations.SetFloat("Forward", forwardVelocity);
 
 		if (inputDirection.magnitude < 0.2f)
 			return;
 
 		transform.rotation = Quaternion.LookRotation(new Vector3(inputDirection.x, 0, inputDirection.y), Vector3.up);
 
-		rigidbody.MovePosition(transform.position + transform.forward * 5f * Time.deltaTime);
+		rigidbody.MovePosition(transform.position + move * Time.deltaTime);
 	}
 }
