75719de959c35feddd92114368f095f17342189d
Pierre-Marie Plans <pmplans@workgroup.aso>
2019-04-29 13:54:28 +0200

    Fix instantiate material

---
+++ Assets/Scripts/InstantiateRendererMat.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstantiateRendererMat : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Renderer>().material = Instantiate<Material>(GetComponent<Renderer>().material);
+    }
+}
